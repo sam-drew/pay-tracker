49d4c169714e3280380efd3309f00d851bcd2954
Sam <sam-drew@outlook.com>
2017-05-02 16:52:02 +0100

    Moved shift calculations to server. Can enter details using webpage.

---
+++ server.py
@@ -0,0 +1,38 @@
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+from tornado.log import enable_pretty_logging
+import logging
+import os.path
+from datetime import datetime
+
+class RootHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+    def post(self):
+        shiftStartDate = str(self.get_argument("shiftStartDate"))
+        shiftStartTime = str(self.get_argument("shiftStartTime"))
+        startDateTime = shiftStartDate + shiftStartTime
+        startDateTime = datetime.strptime(startDateTime, '%Y-%m-%d%H:%M')
+
+        shiftEndDate = str(self.get_argument("shiftEndDate"))
+        shiftEndTime = str(self.get_argument("shiftEndTime"))
+        endDateTime = shiftEndDate + shiftEndTime
+        endDateTime = datetime.strptime(endDateTime, '%Y-%m-%d%H:%M')
+
+        timeDelta = endDateTime - startDateTime
+        tdDecimalHours = (timeDelta.seconds / 3600)
+        breakLength = float(self.get_argument("breakLength"))
+        paidHours = tdDecimalHours - breakLength
+
+enable_pretty_logging()
+app = tornado.web.Application(
+    [(r"/", RootHandler),],
+    template_path = os.path.join(os.path.dirname(__file__), "templates"),
+    )
+
+http_server = tornado.httpserver.HTTPServer(app)
+http_server.listen(8080)
+
+tornado.ioloop.IOLoop.instance().start()
