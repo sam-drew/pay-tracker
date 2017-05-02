e7becf91dbfb37a2a2ad81023984fc25a662ef4e
Sam <sam-drew@outlook.com>
2017-05-02 11:45:52 +0100

    Calculates break length from our handbook and shift length.

--- shiftCalc.py
+++ shiftCalc.py
@@ -4,5 +4,22 @@ startTime = input("Enter start time: ")
 finishTime = input("Enter finish time: ")
 startTime = datetime.strptime(startTime, '%H:%M')
 finishTime = datetime.strptime(finishTime, '%H:%M')
-timedelta = finishTime - startTime
-print(timedelta)
+timeDelta = finishTime - startTime
+timeDeltaSeconds = timeDelta.seconds
+timeDeltaDecimalHours = timeDeltaSeconds / 3600
+if timeDeltaDecimalHours <= 3.75:
+    breakLength = 0
+elif timeDeltaDecimalHours <= 5.75:
+    breakLength = 0.25
+elif timeDeltaDecimalHours <= 7.75:
+    breakLength = 0.5
+elif timeDeltaDecimalHours <= 8.75:
+    breakLength = 0.75
+else:
+    breakLength = 1.5
+print("Break length is calculated to be:", breakLength)
+breakCorrect = input("Is the break length correct? (y/n)")
+while breakCorrect != "y" and breakCorrect != "n":
+    breakCorrect = "Please enter y or n: "
+if breakCorrect == "n":
+    breakLength = input("Please enter the correct length of your break: ")
