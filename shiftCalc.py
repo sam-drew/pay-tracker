aa10b134e773db23b0c4e2c0f1a700ed90e38b72
Sam <sam-drew@outlook.com>
2017-05-02 11:59:04 +0100

    Removed break calculation, get user to enter manually.

--- shiftCalc.py
+++ shiftCalc.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+hourlyPay = float(input("Please enter your hourly rate of pay (as a decimal in £): "))
 startTime = input("Enter start time: ")
 finishTime = input("Enter finish time: ")
 startTime = datetime.strptime(startTime, '%H:%M')
@@ -7,19 +8,7 @@ finishTime = datetime.strptime(finishTime, '%H:%M')
 timeDelta = finishTime - startTime
 timeDeltaSeconds = timeDelta.seconds
 timeDeltaDecimalHours = timeDeltaSeconds / 3600
-if timeDeltaDecimalHours <= 3.75:
-    breakLength = 0
-elif timeDeltaDecimalHours <= 5.75:
-    breakLength = 0.25
-elif timeDeltaDecimalHours <= 7.75:
-    breakLength = 0.5
-elif timeDeltaDecimalHours <= 8.75:
-    breakLength = 0.75
-else:
-    breakLength = 1.5
-print("Break length is calculated to be:", breakLength)
-breakCorrect = input("Is the break length correct? (y/n)")
-while breakCorrect != "y" and breakCorrect != "n":
-    breakCorrect = "Please enter y or n: "
-if breakCorrect == "n":
-    breakLength = input("Please enter the correct length of your break: ")
+breakLength = float(input("Please enter the correct length of your break: "))
+paidHours = timeDeltaDecimalHours - breakLength
+print("You will be paid for", paidHours, "hours.")
+print("For a total of £" + str(paidHours * hourlyPay))
